ae1e9948439ddf1548d3d58a14b12729036c940e
Fazle Bari <fazlebarisn@gmail.com>
2023-09-06 18:31:11 +0600

    update

--- build/subscribe/index.asset.php
+++ build/subscribe/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '463c40a7eff14121e016');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '8eb8dd1943682979b588');
--- build/subscribe/index.js
+++ build/subscribe/index.js
@@ -53,6 +53,31 @@ function Edit({
     name: 'dark',
     color: '#333333'
   }];
+  const gradientColors = [{
+    name: 'Vivid cyan blue to vivid purple',
+    gradient: 'linear-gradient(135deg,rgba(6,147,227,1) 0%,rgb(155,81,224) 100%)',
+    slug: 'vivid-cyan-blue-to-vivid-purple'
+  }, {
+    name: 'Light green cyan to vivid green cyan',
+    gradient: 'linear-gradient(135deg,rgb(122,220,180) 0%,rgb(0,208,130) 100%)',
+    slug: 'light-green-cyan-to-vivid-green-cyan'
+  }, {
+    name: 'Luminous vivid amber to luminous vivid orange',
+    gradient: 'linear-gradient(135deg,rgba(252,185,0,1) 0%,rgba(255,105,0,1) 100%)',
+    slug: 'luminous-vivid-amber-to-luminous-vivid-orange'
+  }, {
+    name: 'Luminous vivid orange to vivid red',
+    gradient: 'linear-gradient(135deg,rgba(255,105,0,1) 0%,rgb(207,46,46) 100%)',
+    slug: 'luminous-vivid-orange-to-vivid-red'
+  }, {
+    name: 'Very light gray to cyan bluish gray',
+    gradient: 'linear-gradient(135deg,rgb(238,238,238) 0%,rgb(169,184,195) 100%)',
+    slug: 'very-light-gray-to-cyan-bluish-gray'
+  }, {
+    name: 'Cool to warm spectrum',
+    gradient: 'linear-gradient(135deg,rgb(74,234,220) 0%,rgb(151,120,209) 20%,rgb(207,42,186) 40%,rgb(238,44,130) 60%,rgb(251,105,98) 80%,rgb(254,248,76) 100%)',
+    slug: 'cool-to-warm-spectrum'
+  }];
   return (0,_wordpress_element__WEBPACK_IMPORTED_MODULE_0__.createElement)("div", {
     ...(0,_wordpress_block_editor__WEBPACK_IMPORTED_MODULE_2__.useBlockProps)()
   }, (0,_wordpress_element__WEBPACK_IMPORTED_MODULE_0__.createElement)(_wordpress_block_editor__WEBPACK_IMPORTED_MODULE_2__.InspectorControls, {
@@ -100,31 +125,7 @@ function Edit({
     onChange: color => setAttributes({
       formBackground: color
     }),
-    gradients: [{
-      name: 'Vivid cyan blue to vivid purple',
-      gradient: 'linear-gradient(135deg,rgba(6,147,227,1) 0%,rgb(155,81,224) 100%)',
-      slug: 'vivid-cyan-blue-to-vivid-purple'
-    }, {
-      name: 'Light green cyan to vivid green cyan',
-      gradient: 'linear-gradient(135deg,rgb(122,220,180) 0%,rgb(0,208,130) 100%)',
-      slug: 'light-green-cyan-to-vivid-green-cyan'
-    }, {
-      name: 'Luminous vivid amber to luminous vivid orange',
-      gradient: 'linear-gradient(135deg,rgba(252,185,0,1) 0%,rgba(255,105,0,1) 100%)',
-      slug: 'luminous-vivid-amber-to-luminous-vivid-orange'
-    }, {
-      name: 'Luminous vivid orange to vivid red',
-      gradient: 'linear-gradient(135deg,rgba(255,105,0,1) 0%,rgb(207,46,46) 100%)',
-      slug: 'luminous-vivid-orange-to-vivid-red'
-    }, {
-      name: 'Very light gray to cyan bluish gray',
-      gradient: 'linear-gradient(135deg,rgb(238,238,238) 0%,rgb(169,184,195) 100%)',
-      slug: 'very-light-gray-to-cyan-bluish-gray'
-    }, {
-      name: 'Cool to warm spectrum',
-      gradient: 'linear-gradient(135deg,rgb(74,234,220) 0%,rgb(151,120,209) 20%,rgb(207,42,186) 40%,rgb(238,44,130) 60%,rgb(251,105,98) 80%,rgb(254,248,76) 100%)',
-      slug: 'cool-to-warm-spectrum'
-    }]
+    gradients: gradientColors
   }))), (0,_wordpress_element__WEBPACK_IMPORTED_MODULE_0__.createElement)(_wordpress_block_editor__WEBPACK_IMPORTED_MODULE_2__.PanelColorSettings, {
     title: (0,_wordpress_i18n__WEBPACK_IMPORTED_MODULE_1__.__)('Color Settings', 'fbs-block'),
     initialOpen: false,

c2062fd4772a959c21e0b6e4636d40867a761c7e
Fazle Bari <fazlebarisn@gmail.com>
2023-09-06 18:20:14 +0600

    add gradient

--- build/subscribe/index.asset.php
+++ build/subscribe/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '53bb523c5153805be5fe');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '463c40a7eff14121e016');

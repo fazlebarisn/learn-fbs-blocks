67b8c64d82648114682c7c844b20f77182cf5735
Fazle Bari <fazlebarisn@gmail.com>
2023-09-17 10:58:12 +0600

    new

--- assets/fbs-block.js
+++ assets/fbs-block.js
@@ -2,16 +2,6 @@
 	$(document).ready(function () {
 
 		$("body").on("click", ".fbs-pricing-table .checkbox", function (e) {
-      
-      if( this.checked ){
-        $('#basic').text('$199.99');
-        $('#professional').text('$249.99');
-        $('#master').text('$399.99');
-      }else{
-        $('#basic').text('$19.99');
-        $('#professional').text('$24.99');
-        $('#master').text('$39.99'); 
-      }
 
 		});
 
